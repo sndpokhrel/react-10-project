ebc2f56f042a57c99d65553a265f4169adda75e2
SP_doesnot_works <sandypokhrel09@gmail.com>
2023-07-25 14:44:50 +0545

    App file

--- react_contact_page/src/App.jsx
+++ react_contact_page/src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
+import ContactHeader from "./components/ContactHeader/ContactHeader";
+import ContactForm from "./components/ContactForm/ContactForm";
 
 function App() {
   return (
     <>
       <Navigation />
+      <main className="main_container">
+        <ContactHeader />
+        <ContactForm />
+      </main>
     </>
   );
 }
