c8817baf9ce34812220048ec831600ca90060fa4
SP_doesnot_works <sandypokhrel09@gmail.com>
2023-07-25 14:44:59 +0545

    Button module

---
+++ react_contact_page/src/components/Button/Button.jsx
@@ -0,0 +1,17 @@
+import styles from "./Button.module.css";
+
+function Button(props) {
+  const { isOutline, icon, text, ...rest } = props;
+
+  return (
+    <button
+      {...rest}
+      className={isOutline ? styles.outline_btn : styles.primary_btn}
+    >
+      {icon}
+      {text}
+    </button>
+  );
+}
+
+export default Button;
