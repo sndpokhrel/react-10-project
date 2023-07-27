f679f275a2bbcdac8197ca7276871cf5d6b6f214
SP_doesnot_works <sandypokhrel09@gmail.com>
2023-07-27 17:49:23 +0545

    no query component

---
+++ firebase_contact/src/components/NotFoundContact.jsx
@@ -0,0 +1,12 @@
+function NotFoundContact() {
+  return (
+    <div className="flex justify-center items-center h-[80vh] gap-4">
+      <div>
+        <img src="/contact.png" alt="not found" />
+      </div>
+      <h3 className="text-white text-2xl font-semibold">Contact Not Found</h3>
+    </div>
+  );
+}
+
+export default NotFoundContact;
