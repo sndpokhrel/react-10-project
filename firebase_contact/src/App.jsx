993c90decfa27453fe795a14b062f31d88fb22d7
SP_doesnot_works <sandypokhrel09@gmail.com>
2023-07-27 17:47:00 +0545

    App css and mian file

---
+++ firebase_contact/src/App.jsx
@@ -0,0 +1,95 @@
+import Navbar from "./components/Navbar";
+import { FiSearch } from "react-icons/fi";
+import { AiFillPlusCircle } from "react-icons/ai";
+import { useEffect, useState } from "react";
+import { collection, onSnapshot } from "firebase/firestore";
+import { db } from "./config/firebase";
+import ContactCard from "./components/ContactCard";
+import AddAndUpdateContact from "./components/AddAndUpdateContact";
+import useDisclose from "./hooks/useDisclose";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import NotFoundContact from "./components/NotFoundContact";
+
+const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const { isOpen, onClose, onOpen } = useDisclose();
+
+  useEffect(() => {
+    const getContacts = async () => {
+      try {
+        const contactsRef = collection(db, "contacts");
+        onSnapshot(contactsRef, (snapshot) => {
+          const contactLists = snapshot.docs.map((doc) => {
+            return {
+              id: doc.id,
+              ...doc.data(),
+            };
+          });
+          setContacts(contactLists);
+          return contactLists;
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getContacts();
+  }, []);
+
+  const filterContacts = (e) => {
+    const value = e.target.value;
+    const contactsRef = collection(db, "contacts");
+    onSnapshot(contactsRef, (snapshot) => {
+      const contactLists = snapshot.docs.map((doc) => {
+        return {
+          id: doc.id,
+          ...doc.data(),
+        };
+      });
+
+      const filteredContacts = contactLists.filter((contact) =>
+        contact.name.toLowerCase().includes(value.toLowerCase())
+      );
+
+      setContacts(filteredContacts);
+
+      return filteredContacts;
+    });
+  };
+
+  return (
+    <>
+      <div className="max-w-[370px] mx-auto px-4">
+        <Navbar />
+        <div className="flex gap-2">
+          <div className="relative flex items-center flex-grow">
+            <FiSearch className="text-white text-3xl absolute ml-1" />
+            <input
+              onChange={filterContacts}
+              type="text"
+              className="text-white pl-9 flex-grow h-10 bg-transparent border border-white rounded-md "
+            />
+          </div>
+          <AiFillPlusCircle
+            onClick={onOpen}
+            className="text-white text-5xl cursor-pointer"
+          />
+        </div>
+        <div className="mt-4 gap-3 flex flex-col">
+          {contacts.length <= 0 ? (
+            <NotFoundContact />
+          ) : (
+            contacts.map((contact) => (
+              <ContactCard key={contact.id} contact={contact} />
+            ))
+          )}
+        </div>
+      </div>
+      <AddAndUpdateContact onClose={onClose} isOpen={isOpen} />
+      <ToastContainer position="bottom-center" />
+    </>
+  );
+};
+
+export default App;
